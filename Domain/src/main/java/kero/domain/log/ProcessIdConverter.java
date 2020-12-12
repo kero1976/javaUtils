7b40127621e1babf3a3b026aa4a08b071917f9aa
kero <kero@DESKTOP-TD25L14.localdomain>
2020-12-12 12:38:01 +0900

    無駄な空行があったので削除

--- Domain/src/main/java/kero/domain/log/ProcessIdConverter.java
+++ Domain/src/main/java/kero/domain/log/ProcessIdConverter.java
@@ -1,6 +1,5 @@
 package kero.domain.log;
 
-
 import ch.qos.logback.classic.pattern.ClassicConverter;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import java.lang.management.ManagementFactory;
